840979b19b6a95309f86f61fbbff777582fba32b
Timothy Kropp <timothy.kropp@fcbup.de>
2025-05-15 13:33:53 +0200

    adds recieved_data endpoint

--- Server2/server.py
+++ Server2/server.py
@@ -1,14 +1,26 @@
-from flask import Flask
-
+import os
+import json
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 @app.route('/', methods=["GET"])
 def home():
     return "running"
 
-@app.route('/ping', methods=["GET"])
-def ping():
-    return "pong"
+@app.route('/recieve_data', methods=["POST"])
+def recieve_data():
+    data = request.get_json()
+    if not data:
+        return jsonify("Error: keine Daten empfangen"), 400
+
+    user_data_path = os.path.join(os.path.dirname(__file__), "Recieved_Data", "users.json")
+
+    filepath = os.path.join(user_data_path, "users_recieved.json")
+    with open(filepath, "w") as f:
+        json.dump(data, f, indent=4)
+
+    return jsonify("Daten sind erfolgreich angekommen")
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)

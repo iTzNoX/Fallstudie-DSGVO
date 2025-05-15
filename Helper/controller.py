0eaf8a4fadcea1e0dd66dae412e8f11779c24a65
Timothy Kropp <timothy.kropp@fcbup.de>
2025-05-15 14:23:33 +0200

    adds copy_received_data_from_container to controller

--- Helper/controller.py
+++ Helper/controller.py
@@ -1,6 +1,9 @@
 import requests
+import subprocess
+import os
 
 SERVER1_URL = "http://localhost:5001"
+SERVER2_URL = "http://localhost:5002"
 
 def send_data():
     url = f"{SERVER1_URL}/send_data"
@@ -13,4 +16,22 @@ def send_data():
         print(e)
         return None
 
+def copy_received_data_from_container(container_name):
+    container_path = "/app/Received_Data/users_received.json"
+    local_path = os.path.join(os.path.dirname(os.getcwd()), "Server2", "Received_Data")
+
+    try:
+        subprocess.run(
+            ["docker", "cp", f"{container_name}:{container_path}", local_path],
+            check = True)
+
+        print("Ordner im Container wurde erfolgreich ins Projekt kopiert")
+
+    except subprocess.CalledProcessError as e:
+        print(str(e))
+
+    except Exception as e:
+        print(str(e))
+
 send_data()
+copy_received_data_from_container("server2")

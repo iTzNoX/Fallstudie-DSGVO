50200d937820cc4045b70ecd27804e10acdbe716
Timothy Kropp <timothy.kropp@fcbup.de>
2025-05-22 19:46:46 +0200

    adds a test to validate server authentication

--- Tests/TLS/Connection.py
+++ Tests/TLS/Connection.py
@@ -1,7 +1,10 @@
+import json
 import os
 import socket
 import ssl
 import pytest
+import requests
+
 
 @pytest.mark.parametrize("host,port", [("localhost", 5001), ("localhost", 5002)])
 def test_tls_connection(host, port):
@@ -11,3 +14,21 @@ def test_tls_connection(host, port):
             protocol_version = ssock.version()
             assert protocol_version is not None, "Keine TLS-Verbindung aufgebaut"
             print(f"TLS Version: {protocol_version}")
+
+@pytest.mark.parametrize("api_key, expected_status", [(
+        json.load(open(os.path.join(os.path.dirname(__file__), "..", "..", "Certs", "header_key.json")))["api_key"],
+        200),
+        ("0000000000000000000000000000000000000000000000000000000000000000",
+        401)])
+def test_api_key_auth(api_key, expected_status):
+    cert_path = os.path.join(os.path.dirname(__file__), "..", "..", "Certs", "certificates.pem")
+    server2_url = "https://localhost:5002/receive_data"
+
+    headers = {
+        "Authorization": f"Bearer {api_key}",
+        "Content-Type": "application/json"
+    }
+    test_data = {"test": "api key auth"}
+
+    response = requests.post(server2_url, json=test_data, headers=headers, verify=cert_path)
+    assert response.status_code == expected_status
